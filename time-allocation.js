98e8cebff133773baeb69d410d5df0a96d44f8d4
Facundo Perez <facundo.perez@lightit.io>
2023-02-13 02:11:35 -0300

    Challenge improvements

--- time-allocation.js
+++ time-allocation.js
@@ -1,9 +1,33 @@
-function solution(numbers, target) {
-    if (numbers.length === 0) {
-        return target === 0;
+const search = (target, numbers, cache) => {
+    if (target === 0) {
+        return true;
+    }
+    if (target < 0 || numbers.length === 0) {
+        return false;
+    }
+
+    const cacheKey = `${target},${numbers.length}`;
+    if (cacheKey in cache) {
+        return cache[cacheKey]
     }
 
     const [first, ...rest] = numbers;
+    const found = search(target - first, rest, cache) ||
+        search(target, rest, cache);
 
-    return solution(rest, target - first) || solution(rest, target);
+    cache[cacheKey] = found
+
+    return found;
+};
+
+function solution(target, numbers) {
+    if (target === 0 || numbers.length === 0) {
+        return target === 0;
+    }
+
+    const cache = {};
+
+    const found = search(target, numbers, cache);
+
+    return found
 }
