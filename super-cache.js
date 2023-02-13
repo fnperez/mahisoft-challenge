1fb388fb5334025e8799a33bd6360d3266379d38
Facundo Perez <facundo.perez@lightit.io>
2023-02-13 01:23:19 -0300

    Challenge improvements

--- super-cache.js
+++ super-cache.js
@@ -1,35 +1,59 @@
-function solution(t, input_arr) {
-    const cacheSlots = []
+class LRUCache {
+    constructor(size) {
+        this.deque = []
+        this.index = {}
+        this.size = size
+        this.findings = 0
+        this.maxFindings = Number.MIN_SAFE_INTEGER
+    }
 
-    let timePassedSinceLastUpdate = 0
-    let biggestTime = Number.MIN_SAFE_INTEGER
+    has(id) {
+        return id in this.index
+    }
+
+    put(id) {
+        if (this.has(id)) {
+            this.found()
+            
+            return
+        } 
+
+        this.miss()
+
+        if (this.deque.length == this.size) {
+            const itemId = this.deque.shift()
+
+            delete this.index[itemId]
+        }
+
+        this.deque.push(id)
+        this.index[id] = true
+    }
+
+    assignFindings() {
+        this.maxFindings = this.maxFindings < this.findings ? this.findings : this.maxFindings
+    }
+
+    found() {
+        this.findings++       
+        this.assignFindings()
+    }
+
+    miss() {
+        this.assignFindings()
+        this.findings = 0
+    }
+}
+
+
+function solution(t, input_arr) {
+    const cacheSlots = new LRUCache(t)
 
     input_arr.forEach((id) => {
-        const cacheSize = cacheSlots.length;
-        const presentInCache = cacheSlots.includes(id)
-        
-        if (cacheSize < t && !presentInCache) {
-            cacheSlots.push(id)
-
-            return
-        }
-
-        if (presentInCache) {
-            timePassedSinceLastUpdate++
-
-            return
-        }
-
-        
-        cacheSlots.shift()
-
-        cacheSlots.push(id)
-
-        if (biggestTime < timePassedSinceLastUpdate) {
-            biggestTime = timePassedSinceLastUpdate
-            timePassedSinceLastUpdate = 0
-        }
+        cacheSlots.put(id)
     })
 
-    return biggestTime
+    return cacheSlots.maxFindings
 }
+
+console.log(solution(5, [1, 2, 3, 2, 4, 2, 3, 3, 4, 2, 1, 2, 8, 9]))
